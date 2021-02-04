ca55b4acc9842ff58996e58e7fe56bb4628b764f
Joan <jmdefelippe@gmail.com>
2021-02-03 21:28:29 -0300

    Login y agregar productos

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClsXZl4vaguxpEKCZ-aGX3ujihIkGrc5Y",
+    authDomain: "product-hunt-6af31.firebaseapp.com",
+    projectId: "product-hunt-6af31",
+    storageBucket: "product-hunt-6af31.appspot.com",
+    messagingSenderId: "523278442821",
+    appId: "1:523278442821:web:6dd2696394e29b56ae9e2c"
+};
+
+export default firebaseConfig;
