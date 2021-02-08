576bb73936d928bf4f31c60fc112fda2c2097b29
Joan <jmdefelippe@gmail.com>
2021-02-08 18:34:27 -0300

    Votos, comentarios, orden por creación y votos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import Layout from '../components/layout/Layout';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
-const Home = () => (
-  <div>
-    <Layout>
-      <h1>Inicio</h1>
-    </Layout>
-  </div>
-)
+const Home = () => {
+
+  const { productos } = useProductos('creado');
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+            <div className="contenedor">
+                <ul className="bg-white">
+                    {productos.map(producto => (
+                        <DetallesProducto
+                            key={producto.id}
+                            producto={producto}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
 
 export default Home
